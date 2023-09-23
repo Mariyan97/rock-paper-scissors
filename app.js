183f9a4782f0d6a0619cae812503e58176e86faf
Mariyan97 <webdev.mvmnt@gmail.com>
2023-09-23 19:13:38 +0300

    Mariyan: the program work fine , added the compare function too.

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-const player = prompt("Please choose between Rock , Paper , Scissors");
-//computer selection
+const playerSelection = prompt("Please choose between Rock , Paper , Scissors");
 
-let computer = Math.random();
-if (computer <= 0.3333) {
-  computer = "rock";
-} else if (computer > 0.3333 && computer < 0.6666) {
-  computer = "paper";
+//computer selection
+let computerSelection = Math.random();
+if (computerSelection <= 0.3333) {
+  computerSelection = "rock";
+} else if (computerSelection > 0.3333 && computerSelection < 0.6666) {
+  computerSelection = "paper";
 } else {
-  computer = "scissors";
+  computerSelection = "scissors";
 }
 
-console.log(player + "  " + computer);
+//compare function;
+
+function compare(player, computer) {
+  if (player === computer) {
+    return "Draw !";
+  } else if (player === "rock" && computer === "paper") {
+    return "Computer wins!";
+  } else if (player === "rock" && computer === "scissors") {
+    return "Player wins!";
+  } else if (player === "paper" && computer === "rock") {
+    return "Player wins!";
+  } else if (player === "paper" && computer === "scissors") {
+    return "Computer wins!";
+  } else if (player === "scissors" && computer === "paper") {
+    return "Player wins!";
+  } else if (player === "scissors" && computer === "rock") {
+    return "Computer wins!";
+  } else {
+    return "Something is not right!";
+  }
+}
+
+let result = compare(playerSelection, computerSelection);
+
+console.log(playerSelection + " vs " + computerSelection, result);
