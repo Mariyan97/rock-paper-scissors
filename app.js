f405266dc7b1fa50f30459aa469c023f1627abe5
Mariyan97 <webdev.mvmnt@gmail.com>
2023-10-02 18:07:29 +0300

    Mariyan: Added little bit of css to see how its gonna look like. I will finish the design today

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 let playerPoints = 0;
 let computerPoints = 0;
+for (let round = 1; round <= 5; round++) {
+  function playRound(playerSelection) {
+    let toLowerCase = playerSelection.toLowerCase();
+    let computerSelection = Math.random();
 
-function playRound(playerSelection) {
-  let toLowerCase = playerSelection.toLowerCase();
-  let computerSelection = Math.random();
-
-  if (computerSelection <= 0.3333) {
-    computerSelection = "rock";
-  } else if (computerSelection > 0.3333 && computerSelection < 0.6666) {
-    computerSelection = "paper";
-  } else {
-    computerSelection = "scissors";
-  }
-
-  function compare(player, computer) {
-    if (player === computer) {
-      return "Draw!";
-    } else if (
-      (player === "rock" && computer === "scissors") ||
-      (player === "paper" && computer === "rock") ||
-      (player === "scissors" && computer === "paper")
-    ) {
-      playerPoints++;
-      return "Player wins!";
+    if (computerSelection <= 0.3333) {
+      computerSelection = "rock";
+    } else if (computerSelection > 0.3333 && computerSelection < 0.6666) {
+      computerSelection = "paper";
     } else {
-      computerPoints++;
-      return "Computer wins!";
+      computerSelection = "scissors";
     }
-  }
 
-  let result = compare(toLowerCase, computerSelection);
-  console.log();
-  document.getElementById("result").textContent = `Result ${result}`;
-  document.getElementById(
-    "score"
-  ).textContent = `Score: ${playerPoints} vs ${computerPoints}`;
+    function compare(player, computer) {
+      if (player === computer) {
+        return "Draw!";
+      } else if (
+        (player === "rock" && computer === "scissors") ||
+        (player === "paper" && computer === "rock") ||
+        (player === "scissors" && computer === "paper")
+      ) {
+        playerPoints++;
+        return "Player wins!";
+      } else {
+        computerPoints++;
+        return "Computer wins!";
+      }
+    }
+
+    let result = compare(toLowerCase, computerSelection);
+    console.log();
+    document.getElementById("result").textContent = `${result}`;
+    document.getElementById(
+      "score"
+    ).textContent = `${playerPoints} : ${computerPoints}`;
+  }
 }
 document
   .getElementById("rock")
